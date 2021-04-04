33468cf64c6d7861ada939ab996026186402e4a3
Daniel Luiz <danielluizthiessen@gmail.com>
2021-04-04 15:54:13 -0300

    Finalizei confirmação de pedido com envio no whatsapp

--- scripts.js
+++ scripts.js
@@ -1,46 +1,54 @@
 
-let cont1 = 0;
 let cont2 = 0;
 let cont3 = 0;
 
+let comida = 0;
+let bebida = 0;
+let sobremesa = 0;
+
+let nome = ".a";
+let nome2 = ".a";
+let nome3 = ".a";
+let custoTotal = "aa";
+
 function selecaoA (num){
-    if(cont1 != 0){
+    if(comida != 0){
     var anterior = document.querySelector(".selecionadoA");
     anterior.classList.remove("selecionadoA");
     }
     let nome = ".a"+num;
     const elemento = document.querySelector(nome);
     elemento.classList.add("selecionadoA");
-    cont1 ++;
+    comida = num;
     final();
 }
 
 function selecaoB (num){
-    if(cont2 != 0){
+    if(bebida != 0){
     var anterior = document.querySelector(".selecionadoB");
     anterior.classList.remove("selecionadoB");
     }
     let nome = ".b"+num;
     const elemento = document.querySelector(nome);
     elemento.classList.add("selecionadoB");
-    cont2 ++;
+    bebida = num;
     final();
 }
 
 function selecaoC (num){
-    if(cont3 != 0){
+    if(sobremesa != 0){
     var anterior = document.querySelector(".selecionadoC");
     anterior.classList.remove("selecionadoC");
     }
     let nome = ".c"+num;
     const elemento = document.querySelector(nome);
     elemento.classList.add("selecionadoC");
-    cont3 ++;
+    sobremesa = num;
     final();
 }
 
 function final () {
-    if(cont1 != 0 && cont2 !=0 && cont3 != 0){
+    if(comida != 0 && bebida !=0 && sobremesa != 0){
         const piso = document.querySelector(".pedir");
         piso.classList.add("pronto");
         piso.innerHTML = "Fechar Pedido";
@@ -53,6 +61,59 @@ function fecharPedido(){
     aux.classList.add("apagado");
     aux = document.querySelector(".confirmar");
     aux.classList.add("exibir");
+    confirmar();
+}
+
+function confirmar() {
+    
+    var aux = document.querySelector(".precox"+comida);
+    let preco = aux.innerHTML;
+    var mudar = document.querySelector(".apreco");
+    mudar.innerHTML = preco;
+    preco = preco.replace(/,/g, '.');
+    preco = parseFloat(preco);
+
+    aux = document.querySelector(".nomex"+comida);
+    nome = aux.innerHTML;
+    mudar = document.querySelector(".anome");
+    mudar.innerHTML = nome;
+
+    var aux2 = document.querySelector(".precoy"+bebida);
+    let preco2 = aux2.innerHTML;
+    var mudar2 = document.querySelector(".bpreco");
+    mudar2.innerHTML = preco2;
+    preco2 = preco2.replace(/,/g, '.');
+    preco2 = parseFloat(preco2);
+
+    aux2 = document.querySelector(".nomey"+bebida);
+    nome2 = aux2.innerHTML;
+    mudar2 = document.querySelector(".bnome");
+    mudar2.innerHTML = nome2;
+    
+    var aux3 = document.querySelector(".precoz"+sobremesa);
+    let preco3 = aux3.innerHTML;
+    var mudar3 = document.querySelector(".cpreco");
+    mudar3.innerHTML = preco3;
+    preco3 = preco3.replace(/,/g, '.');
+    preco3 = parseFloat(preco3);
+
+    aux3 = document.querySelector(".nomez"+sobremesa);
+    nome3 = aux3.innerHTML;
+    mudar3 = document.querySelector(".cnome");
+    mudar3.innerHTML = nome3;
+
+    var soma = (preco3 + preco2 + preco);
+    soma = soma.toFixed(2);
+    custoTotal = soma.toString();
+    custoTotal = custoTotal.replace(".", ",");
+    var aux4 = document.querySelector(".total");
+    aux4.innerHTML = custoTotal;
 }
 
 
+function abrirLink(){
+    let pedido = "Olá, gostaria de fazer o pedido:\n -Prato: " + nome + "\n -Bebida: " + nome2 + "\n -Sobremesa: " + nome3 + "\n Total: R$" + custoTotal + "\n";
+    let url = encodeURIComponent(pedido);
+    let link = "https://wa.me/5561982621155?text=" + url;
+    window.open(link);
+}
